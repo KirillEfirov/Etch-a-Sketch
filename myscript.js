5a325ed79d82d1d81a4e5f161134ba015b1dedb2
Kirill <shishkokirill.fudosin@gmail.com>
2022-09-05 21:53:03 +0300

    Size of grid can be changed by user

--- myscript.js
+++ myscript.js
@@ -5,7 +5,8 @@ document.body.onmouseup = () => (mouseDown = false);
 const paintContainer = document.querySelector('.paintContainer');
 /*Szie by default*/ 
 const grid = {rows: 64, cols: 64};
-const total = grid.cols * grid.rows;
+let total = grid.cols * grid.rows;
+let rememberValue = total;
 
 
 function createGrid(total) {
@@ -21,35 +22,22 @@ window.onload = createGrid(total);
 
 
 
-const input = document.querySelector('input');
-input.addEventListener('change', getValue);
-let rememberValue = 64;
-
-function getValue(e) {
-    rememberValue = e.target.value;
-    console.log(rememberValue);
-}
-
-
-
-
-
 let box = document.getElementsByClassName('box');
-function paintBlack() {
-        for(const each of box) {
-            each.addEventListener('mouseover', (e) => {
-                if (e.type === 'mouseover' && !mouseDown) return;
-                    each.setAttribute('style', 'background: black'); 
-            });
 
-            each.addEventListener('mouseDown', (e) => {
-                if (e.type === 'mouseover' && !mouseDown) return;
-                    each.setAttribute('style', 'background: black'); 
-            });
-        }
+function paintBlack() {
+    for(const each of box) {
+        each.addEventListener('mouseover', (e) => {
+            if (e.type === 'mouseover' && !mouseDown) return;
+                each.setAttribute('style', 'background: black'); 
+        });
+
+        each.addEventListener('mouseDown', (e) => {
+            if (e.type === 'mouseover' && !mouseDown) return;
+                each.setAttribute('style', 'background: black'); 
+        });
+    }
 }
 window.onload = paintBlack();
-
 
 
 const black = document.querySelector('.black');
@@ -62,15 +50,34 @@ const eraser = document.querySelector('.eraser');
 eraser.addEventListener('click', Eraser);
 
 function Eraser() {
-    for(const each of box) {
-        each.addEventListener('mouseover', (e) => {
-            if (e.type === 'mouseover' && !mouseDown) return;
-                each.setAttribute('style', 'background: white'); 
-        });
+for(const each of box) {
+    each.addEventListener('mouseover', (e) => {
+        if (e.type === 'mouseover' && !mouseDown) return;
+            each.setAttribute('style', 'background: white'); 
+    });
 
-        each.addEventListener('mouseDown', (e) => {
-            if (e.type === 'mouseover' && !mouseDown) return;
-                each.setAttribute('style', 'background: white'); 
-        });
-    }
+    each.addEventListener('mouseDown', (e) => {
+        if (e.type === 'mouseover' && !mouseDown) return;
+            each.setAttribute('style', 'background: white'); 
+    });
 }
+}
+
+
+
+function getValue(e) {
+    rememberValue = e.target.value;
+    console.log(rememberValue);
+    grid.cols = e.target.value;
+    return rememberValue;
+}
+
+
+function newGrid() {
+        paintContainer.innerHTML = '';
+        createGrid(grid.cols**2);
+}
+
+const input = document.querySelector('input');
+input.addEventListener('change', getValue);
+input.addEventListener('change', newGrid);
